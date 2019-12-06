925b1fb65f5042e0a37dd64593c8ccf6ba4a58c4
Kuokkane <tonikuokkanen@hotmail.com>
2019-12-07 00:50:44 +0200

    added delete functionality

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.addtask, name='addtask'),
-
+    path('delete/<list_id>', views.delete, name='delete'),
 ]
--- taskapp/views.py
+++ taskapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.http import HttpResponseRedirect
 
-
+#ottaa käyttäjän syötteen ja tallentaa sen tietokantaan ja listaa kaikki tehtävät
 def addtask(request):
-
+    
 	if request.method == 'POST':
 		form = ListForm(request.POST or None)
 
@@ -17,3 +18,9 @@ def addtask(request):
 	else:
 		tasklist = List.objects.all
 		return render(request, 'addtask.html', {'tasklist': tasklist})
+
+def delete(request, list_id):
+    task = List.objects.get(pk=list_id)
+    task.delete()
+
+    return redirect('addtask')
